4d76470d9f90921b0993453790487fbc1f1a845a
Ilya <kurbatov-work@mail.ru>
2021-02-06 02:04:57 +1100

    Initial commit

---
+++ app/src/main/java/itinbiz/progsi/mytest/MainActivity.java
@@ -0,0 +1,45 @@
+package itinbiz.progsi.mytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.onesignal.OneSignal;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String ONESIGNAL_APP_ID = "5f6725b7-9693-4ca7-8039-3d7f689fac97";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.salesforce.com");
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @SuppressWarnings("deprecation") @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @TargetApi(Build.VERSION_CODES.N) @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        };
+        webView.setWebViewClient(webViewClient);
+    }
+}
